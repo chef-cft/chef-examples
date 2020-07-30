3c2a8faaca48c91b4c97e916bbe409d029bd08fb
Dina Muscanell <dmuscanell@chef.io>
2020-07-30 13:24:13 -0400

    Updating provision script to update packages and install make dependencies; updating readme

--- examples/vagrant/chef-infra-server/provision.sh
+++ examples/vagrant/chef-infra-server/provision.sh
@@ -3,14 +3,29 @@ OS_TYPE=$2
 OS_VERSION=$3
 
 echo "*************************************************************************"
+echo "Updating packages..."
+echo "*************************************************************************"
+if [[ $OS_TYPE == *'ubuntu'* ]]
+then
+  sudo apt-get update
+  sudo apt-get -y install build-essential
+elif [[ $OS_TYPE != *'rhel'* ]]
+then
+    sudo yum update -y
+    sudo yum install gcc gcc-c++ make
+fi
+
+echo "*************************************************************************"
 echo "Downloading & Installing Chef Server Version: $CHEF_SERVER_VERSION"
 echo "*************************************************************************"
-
 if [[ $OS_TYPE == *'ubuntu'* ]]
 then
   wget https://packages.chef.io/files/stable/chef-server/$CHEF_SERVER_VERSION/ubuntu/$OS_VERSION/chef-server-core_$CHEF_SERVER_VERSION-1_amd64.deb
   sudo dpkg -i ./chef-server-core_$CHEF_SERVER_VERSION-1_amd64.deb
-elif [ $OS_TYPE == 'rhel8' ]
+  echo "*************************************************************************"
+  echo "Updating apt-get and installing build-essentials"
+  echo "*************************************************************************"
+elif [[ $OS_TYPE == 'rhel8' || $OS_TYPE == 'centos8' ]]
 then
   wget https://packages.chef.io/files/stable/chef-server/$CHEF_SERVER_VERSION/el/$OS_VERSION/chef-server-core-$CHEF_SERVER_VERSION-1.el7.x86_64.rpm
   rpm -Uvh ./chef-server-core-$CHEF_SERVER_VERSION-1.el7.x86_64.rpm

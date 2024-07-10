76d6e1d0158f0a05b3de0ea87620798165592bfd
akshayparvatikar174 <apravati@progress.com>
2024-07-10 20:46:55 +0530

    Added cookbook for Polyfill Vulnerability (#82)
    * Added cookbook for Polyfill Vulnerability
    
    * Updated recipe
    
    * Delete examples/polyfill/attributes directory

---
+++ examples/polyfill/recipes/default.rb
@@ -0,0 +1,21 @@
+automat_dir = '/hab/pkgs/chef/automate-ui/2.0.0'
+unless ::File.directory?(automat_dir)
+  raise "Directory #{automat_dir} is missing!"
+end
+files = ::Dir.glob(::File.join(automat_dir, '*'))
+parent_dir = ::File.basename(files.first) # => '20240305055235'
+ 
+ 
+file_names_tobe_deleted = %w(collection chef)
+ 
+ 
+file_names_tobe_deleted.each do |f|
+  file_to_be_deleted = "#{automat_dir}/#{parent_dir}/dist/assets/chef-ui-library/#{f}/sandbox.html"
+  file file_to_be_deleted do
+    action :delete
+  end
+end
+file_to_be_created = '/var/log/chef-quarantine-polyfill'
+file file_to_be_created do
+  action :create
+end
